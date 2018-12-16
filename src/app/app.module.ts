32543f6e11ce859e34c73003ffa49c7ccd1c016d
Rob Hill <robert.hill0122@gmail.com>
2018-12-16 14:09:16 -0700

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,14 @@ import {ReactiveFormsModule} from '@angular/forms'
 import {ReactiveFormComponent} from './reactive-form/reactive-form.component'
 import {AuthFormComponent} from './auth-form/auth-form.component'
 import {AsyncValidatorExampleComponent} from './async-validator-example/async-validator-example.component'
+import {RouterModule, Routes} from "@angular/router"
+
+const routes: Routes = [
+  {path: "", redirectTo: "auth", pathMatch: "full"},
+  {path: "auth", component: AuthFormComponent},
+  {path: "dog-builder", component: ReactiveFormComponent},
+  {path: "async-validation", component: AsyncValidatorExampleComponent},
+]
 
 @NgModule({
   declarations: [
@@ -16,7 +24,8 @@ import {AsyncValidatorExampleComponent} from './async-validator-example/async-va
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
